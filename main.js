f28695f000fa8b15b89e1de9c9a99e1411b9e331
Cocoandkratos <81406537+Cocoandkratos@users.noreply.github.com>
2021-12-17 13:17:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload()
+{
+
+}
+
+
+
+
+function setup()
+{
+canvas=createCanvas(300,300);
+canvas.center();
+}
+
+
+
+
+
+
+function draw()
+{
+
+}
+
+
+
+
+
+function take_snapshot()
+{
+    save('Aaniya.png')
+}
